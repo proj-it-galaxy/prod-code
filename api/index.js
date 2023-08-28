93b5b03e22d83fc13df35b36699a291990baf66d
Abdul Ahad Sheikh <95898825+gitabdulahad@users.noreply.github.com>
2023-08-28 14:26:11 +0530

    Login functionality implemented

--- api/index.js
+++ api/index.js
@@ -4,8 +4,10 @@ const mongoose = require("mongoose");
 const User = require('./models/User')
 const bcrypt = require('bcryptjs');
 const app = express();
+const jwt = require('jsonwebtoken');
 
 const salt = bcrypt.genSaltSync(10);
+const secret = 'asanfhkjhaiugtb1937651gaonf';
 
 app.use(cors());
 app.use(express.json());
@@ -17,13 +19,24 @@ app.post('/register',async (req, res) => {
     try{
         const userDoc = await User.create({
             username,
-            password:bcrypt.hashSync(password,salt)
+            password:bcrypt.hashSync(password,salt),
         });
         res.json(userDoc);
     }catch(e){
         res.status(400).json(e);
     }
 
+});
+
+app.post('/login' , async (req,res) => {
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if(passOk){
+
+    }else{
+        res.status(400).json('Wrong credentials')
+    }
 });
 
 app.listen(4000);
