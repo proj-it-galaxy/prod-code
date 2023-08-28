97006a58c0900dcd040da87f7a6ec003fe82274e
Abdul Ahad Sheikh <95898825+gitabdulahad@users.noreply.github.com>
2023-08-28 13:12:07 +0530

    DB Connectivity Established for User Registration

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,10 +1,30 @@
+import { useState } from "react";
+
 export default function RegisterPage(){
-    return(
+    const [username, serUsername] = useState('');
+    const [password, setPassword] = useState('');
+    async function register(ev){
+        ev.preventDefault();
+        await fetch('http://localhost:4000/register',{
+            method: 'POST',
+            body: JSON.stringify({username,password}),
+            headers: {'Content-Type':'application/json'},
+        })
+    }
+        return(
         <div>
-            <form className="register">
+            <form className="register" onSubmit={register}>
                 <h1>Registration Page</h1>
-                <input type="text" placeholder="username" />
-                <input type="password" placeholder="password" />
+                <input type="text" 
+                    placeholder="username" 
+                    value={username} 
+                    onChange={ev => serUsername(ev.target.value)} 
+                    />
+                <input type="password" 
+                    placeholder="password" 
+                    value={password}
+                    onChange={ev => setPassword(ev.target.value)}
+                    />
                 <button>Register</button>
             </form>
         </div>
