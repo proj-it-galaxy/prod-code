1c8cf27723feee3549d4702dd7fd6a661844f549
Abdul Ahad Sheikh <95898825+gitabdulahad@users.noreply.github.com>
2023-08-28 14:35:42 +0530

    Session handling using jwt implemented

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const jwt = require('jsonwebtoken');
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asanfhkjhaiugtb1937651gaonf';
 
-app.use(cors());
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://ahad:m0cDpmzqYDDUj7Zi@clusterx.m1n2ijj.mongodb.net/?retryWrites=true&w=majority');
