5c66d6779591e20505a557f0f6cb0b37542f3485
Abdul Ahad Sheikh <95898825+gitabdulahad@users.noreply.github.com>
2023-08-28 16:26:26 +0530

    Logout functionality implemented

--- api/index.js
+++ api/index.js
@@ -5,12 +5,14 @@ const User = require('./models/User')
 const bcrypt = require('bcryptjs');
 const app = express();
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asanfhkjhaiugtb1937651gaonf';
 
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect('mongodb+srv://ahad:m0cDpmzqYDDUj7Zi@clusterx.m1n2ijj.mongodb.net/?retryWrites=true&w=majority');
 
@@ -35,11 +37,26 @@ app.post('/login' , async (req,res) => {
     if(passOk){
         jwt.sign({username,id:userDoc._id},secret,{},(err,token) => {
             if(err) throw err;
-            res.cookie('token',token).json('ok');
+            res.cookie('token',token).json({
+                id:userDoc._id,
+                username,
+            });
         });
     }else{
         res.status(400).json('Wrong credentials')
     }
+});
+
+app.get('/profile', (req,res) => {
+    const {token} = req.cookies;
+    jwt.verify(token,secret,{}, (err,info) => {
+        if (err) throw err;
+        res.json(info);
+    });
+});
+
+app.post('/logout', (req,res) => {
+    res.cookie('token', '').json('ok')
 });
 
 app.listen(4000);
