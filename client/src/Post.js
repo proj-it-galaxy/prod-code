9a7bb17147cfe8f94ab8a0b3a11282e781f7515f
Abdul Ahad Sheikh <95898825+gitabdulahad@users.noreply.github.com>
2023-08-28 22:23:11 +0530

    Create Post Functionality implemented

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,17 +1,25 @@
-export default function Post() {
-    return(
+import {formatISO9075} from "date-fns";
+import {Link} from "react-router-dom";
+
+export default function Post({_id,title,summary,cover,content,createdAt,author}) {
+
+  return (
     <div className="post">
-        <div className="image">
-        <img src="https://images.unsplash.com/photo-1683009427598-9c21a169f98f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60"></img>
-        </div>
-        <div className="texts">
-        <h2>Full-house battery backup comming later this year</h2>
+      <div className="image">
+        <Link to={`/post/${_id}`}>
+          <img src={'http://localhost:4000/'+cover} alt=""/>
+        </Link>
+      </div>
+      <div className="texts">
+        <Link to={`/post/${_id}`}>
+        <h2>{title}</h2>
+        </Link>
         <p className="info">
-          <a className="author">Abdul Ahad Sheikh</a>
-          <time>2023-01-03 16:45</time>
+          <a className="author">{author.username}</a>
+          <time>{formatISO9075(new Date(createdAt))}</time>
         </p>
-        <p className="summary">This is the summary part of the Post. This need to be the summary and not just lines from the blog!.</p>
-        </div>
+        <p className="summary">{summary}</p>
+      </div>
     </div>
-    );
+  );
 }
